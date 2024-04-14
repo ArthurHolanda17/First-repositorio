0b9a824d327123118575008bb09aa84d9a548c64
ArthurHolanda17 <148383071+ArthurHolanda17@users.noreply.github.com>
2024-04-14 19:37:11 -0300

    Create Q05.py

---
+++ Listas em Python/Q05.py
@@ -0,0 +1,17 @@
+idades = []
+alturas = []
+
+for i in range(10):
+  idade = int(input("Informe a idade do " + str(i + 1) + "º aluno: "))
+  altura = float(input("Informe a altura do " + str(i + 1) + "º aluno (cm): "))
+  idades.append(idade)
+  alturas.append(altura)
+
+media_altura = sum(alturas) / len(alturas)
+
+alunos_acima_13_baixos = 0
+for i in range(len(idades)):
+  if idades[i] > 13 and alturas[i] < media_altura:
+    alunos_acima_13_baixos += 1
+
+print("\nNúmero de alunos com mais de 13 anos AND altura inferior à média:", alunos_acima_13_baixos)
