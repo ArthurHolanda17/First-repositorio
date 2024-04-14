8add72172c06fc7debb999884b1dba95dfb73ff2
ArthurHolanda17 <148383071+ArthurHolanda17@users.noreply.github.com>
2024-04-14 17:16:18 -0300

    Create Q09.py

---
+++ Estruturas de comparação/Q09.py
@@ -0,0 +1,18 @@
+salario = float(input("Digite o salário do colaborador: "))
+
+if salario <= 280:
+    percentual_aumento = 20
+elif salario <= 700:
+    percentual_aumento = 15
+elif salario <= 1500:
+    percentual_aumento = 10
+else:
+    percentual_aumento = 5
+
+aumento = (percentual_aumento / 100) * salario
+novo_salario = salario + aumento
+
+print("Salário antes do reajuste:", salario)
+print(f"Percentual de aumento aplicado: {percentual_aumento}%")
+print("Valor do aumento: R$", aumento)
+print("Novo salário após o aumento: R$", novo_salario)
