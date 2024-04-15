ccdb96fbcebcbda3533903fe162af296c86f9fcc
Arthur Holanda 💻⚡ <148383071+ArthurHolanda17@users.noreply.github.com>
2024-04-15 20:18:47 -0300

    Delete Estrutura sequencial/Questão 5.py

--- Estrutura sequencial/Questão 5.py
+++
@@ -1,6 +0,0 @@
-# 5 - Faça um Programa que converta metros para centímetros.
-
-print ("Conversão -- METROS for CENTÍMETROS")
-metros = float(input("Digite um valor (em metros): "))
-centim = metros * 100
-print (f"O valor em centímetros é: {centim}")
